e9183bc9d7625459f6658d6b84e30be2e71a8330
mango1010 <201291354+mang01010@users.noreply.github.com>
2025-03-25 14:38:00 +0100

    Update __init__.py
    Added Prompt Load and Save nodes

--- __init__.py
+++ __init__.py
@@ -8,29 +8,35 @@ from .PromptEmbedMango import PromptEmbedMango
 from .CompositeMangoLoader import CompositeMangoLoader
 from .FluxSamplerMango import FluxSamplerMango
 from .FluxGuidanceMango import FluxGuidanceMango
+from .MangoPromptSave import PromptSave
+from .MangoPromptLoad import MangoPromptLoad
 
 NODE_CLASS_MAPPINGS = {
     "MangoTriggerExporter": MangoTriggerExporter,
-    "KSamplerMango": KSamplerMango,
-    "MangoLoader": MangoLoader,
-    "ImageSaverMango": ImageSaverMango,
-    "LatentImageMango": LatentImageMango,
-    "PromptMango": PromptMango,
-    "PromptEmbedMango": PromptEmbedMango,
+    "KSamplerMango":       KSamplerMango,
+    "MangoLoader":         MangoLoader,
+    "ImageSaverMango":     ImageSaverMango,
+    "LatentImageMango":    LatentImageMango,
+    "PromptMango":         PromptMango,
+    "PromptEmbedMango":    PromptEmbedMango,
     "CompositeMangoLoader": CompositeMangoLoader,
-    "FluxSamplerMango": FluxSamplerMango,
-    "FluxGuidanceMango": FluxGuidanceMango,
+    "FluxSamplerMango":    FluxSamplerMango,
+    "FluxGuidanceMango":   FluxGuidanceMango,
+    "PromptSave":          PromptSave,
+    "MangoPromptLoad":     MangoPromptLoad,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "MangoTriggerExporter": "Trigger Exporter (Mango)",
-    "KSamplerMango": "KSampler (Mango)",
-    "MangoLoader": "Loader (Mango)",
-    "ImageSaverMango": "Image Saver (Mango)",
-    "LatentImageMango": "Latent Image (Mango)",
-    "PromptMango": "Prompt (Mango)",
-    "PromptEmbedMango": "Prompt /w Embedding (Mango)",
+    "KSamplerMango":       "KSampler (Mango)",
+    "MangoLoader":         "Loader (Mango)",
+    "ImageSaverMango":     "Image Saver (Mango)",
+    "LatentImageMango":    "Latent Image (Mango)",
+    "PromptMango":         "Prompt (Mango)",
+    "PromptEmbedMango":    "Prompt /w Embedding (Mango)",
     "CompositeMangoLoader": "Diffusion Loader (Mango)",
-    "FluxSamplerMango": "FluxSampler (Mango)",
-    "FluxGuidanceMango": "FluxGuidance (Mango)",
+    "FluxSamplerMango":    "FluxSampler (Mango)",
+    "FluxGuidanceMango":   "FluxGuidance (Mango)",
+    "PromptSave":          "Save Prompt (Mango)",
+    "MangoPromptLoad":     "Load Prompt (Mango)",
 }
